ed7d2637f890c0ab7b8033c668393e095f101d4c
Mike Gehard <mike.gehard@livingsocial.com>
2012-04-12 12:24:34 -0600

    Change to use subject block

--- spec/terminal_spec.rb
+++ spec/terminal_spec.rb
@@ -11,6 +11,8 @@ describe Terminal do
         PricingEntry.new("D", 0.15),
     ]
   }
+
+  subject { Terminal.new pricing_entries }
   describe "when you scan an item with no pricing entry" do
     it "does something"
   end
@@ -18,49 +20,44 @@ describe Terminal do
 
     context "for a product code with only a unit price" do
       it "calculates the total price as quantity * unit price" do
-        terminal = Terminal.new pricing_entries
         2.times do
-          terminal.scan "B"
+          subject.scan "B"
         end
-        terminal.total.should == 24.00
+        subject.total.should == 24.00
       end
     end
     context 'for a product with a unit price and a quantity price' do
       it "calculates the total price using the quantity price first and then adds the unit price for extras" do
-        terminal = Terminal.new pricing_entries
         5.times do
-          terminal.scan 'A'
+          subject.scan 'A'
         end
-        terminal.total.should == 9.00
+        subject.total.should == 9.00
       end
     end
     context "for the series of items ABCDABAA" do
       it "calculates the total amount properly" do
-        terminal = Terminal.new pricing_entries
         %w{A B C D A B A A}.each do |product_code|
-          terminal.scan(product_code)
+          subject.scan(product_code)
         end
-        terminal.total.should == 32.40
+        subject.total.should == 32.40
       end
     end
 
     context "for the series of items CCCCCCC" do
       it "calculates the total amount properly" do
-        terminal = Terminal.new pricing_entries
         7.times do
-          terminal.scan('C')
+          subject.scan('C')
         end
-        terminal.total.should == 7.25
+        subject.total.should == 7.25
       end
     end
 
     context "for the series of items ABCD" do
       it "calculates the total amount properly" do
-        terminal = Terminal.new pricing_entries
         %w{A B C D}.each do |product_code|
-          terminal.scan(product_code)
+          subject.scan(product_code)
         end
-        terminal.total.should == 15.40
+        subject.total.should == 15.40
       end
     end
   end
